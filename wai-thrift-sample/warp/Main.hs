a0e090efd9907432812a00e454de3cf2ac2f7389
Yogesh Sajanikar <yogesh.sajanikar@yahoo.com>
2015-10-30 21:02:03 +0530

    Tested binary protocol over http/wai

--- wai-thrift-sample/warp/Main.hs
+++ wai-thrift-sample/warp/Main.hs
@@ -4,13 +4,14 @@ module Main where
 import GreetingApp
 import qualified Greeting as G
 import Network.Wai.Middleware.Cors
-import Thrift.Protocol.JSON
+import Thrift.Protocol.Binary
 import Thrift.Transport.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.RequestLogger
 
 -- | Sample application that runs warp, and handles thrift protocol
 -- To test it use `curl -X POST -d "[1, \"hello\", 1, 0, {}]" http://localhost:3000`
 
 main :: IO ()
-main = run 3000 $ simpleCors $ thriftApp GreetData JSONProtocol JSONProtocol G.process
+main = run 3000 $ logStdoutDev $ simpleCors $ thriftApp GreetData BinaryProtocol BinaryProtocol G.process
 
